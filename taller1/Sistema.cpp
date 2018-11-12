aedc72628c706aa661b758750453e9db026966cc
patoarayas <pato.araya.gonzalez@gmail.com>
2018-11-12 00:15:02 -0300

    CArga Archivos

--- taller1/Sistema.cpp
+++ taller1/Sistema.cpp
@@ -105,13 +105,13 @@ bool Sistema::EventoRealizado(string id, int cantidadAsistentes)
 	}
 	else {
 		if (evento->getPersonasAsistentes() > evento->getPersonasEsperadas()) {
-			cout << "No válido, las personas asistentes no deben superar a las esperadas." << endl;
+			cout << "No vï¿½lido, las personas asistentes no deben superar a las esperadas." << endl;
 			return false;
 		}
 		else {
 			cout << "Utilidad:  ";
 			cout << evento->utilidad();
-			cout << "  dólares." << endl;
+			cout << "  dï¿½lares." << endl;
 			Administrador* admin = administradores->buscarAdmin(evento->getIdAdministrador());
 			admin->setMonto(admin->getMonto() + (evento->utilidad()*0.5));
 			if (admin->getMonto() <= 0) {
@@ -144,7 +144,7 @@ bool Sistema::CancelarEvento(string id)
 			cout << "El administrador: ";
 			cout << admin->toString();
 			cout << "  fue despedido." << endl;
-			administradores->eliminarPersona(admin->getId());		
+			administradores->eliminarPersona(admin->getId());
 			return true;
 		}
 	}
@@ -168,4 +168,134 @@ void Sistema::Administradores()
 
 Sistema::~Sistema()
 {
+}
+
+bool Sistema::LeerArchivos() {
+
+    ifstream arch1 ("Eventos.txt");
+
+    string line1;
+    if(arch1.is_open()){
+        while(getline(arch1,line1)){
+            stringstream ss (line1);
+
+            string nombre;
+            string ciudad;
+            string idCliente;
+            string idAdmin;
+            string idEvento;
+            string tipo;
+            string estado;
+            string personasEsperadas;
+            getline(ss,nombre,',');
+            getline(ss,ciudad,',');
+            getline(ss,idCliente,',');
+            getline(ss,idAdmin,',');
+            getline(ss,idEvento,',');
+            getline(ss,tipo,',');
+            getline(ss,estado,',');
+            getline(ss,personasEsperadas,',');
+            int esperadas = stoi(personasEsperadas);
+            string personasAsistentes;
+            int asistentes;
+            Evento* nuevoEvento;
+            if(estado == "Realizado"){
+                getline(ss,personasAsistentes,',');
+                asistentes = stoi(personasAsistentes);
+                nuevoEvento=  new Evento(nombre,ciudad,idCliente,idAdmin,idEvento,tipo,estado,esperadas,asistentes);
+            }
+            else{
+                nuevoEvento = new Evento(nombre,ciudad,idCliente,idAdmin,idEvento,tipo,estado,esperadas,0);
+            }
+
+
+            this->eventos->addEvento(*nuevoEvento);
+
+
+        }
+    }
+    else{
+        cout<<"NO SE PUDO ABRIR EL ARCHIVO"<<endl;
+        return false;
+    }
+
+    arch1.close();
+
+    ifstream arch2 ("Administradores.txt");
+    string line2;
+    if(arch2.is_open()){
+
+        while(getline(arch2,line2)){
+            stringstream ss (line2);
+            string nombre;
+            getline(ss,nombre,';');
+            string apellido;
+            getline(ss,apellido,';');
+            string idAdmin;
+            getline(ss,idAdmin,';');
+            string ciudad;
+            getline(ss,ciudad,';');
+            string montoStr;
+            getline(ss,montoStr,';');
+            Administrador* nuevoAdmin = new Administrador(nombre,apellido,ciudad,stoi(montoStr),0);
+
+            while(!ss.eof()){
+
+                string idEvento;
+                getline(ss,idEvento,';');
+                //TODO: FALTA AGREGAR EVENTOS A LA LISTA DE EVENTOS DEL ADMINISTRADOR
+
+            }
+            //TODO: Slicing
+            this->administradores->addPersona(*nuevoAdmin);
+        }
+
+    }
+    else{
+        cout<<"NO SE PUDO CARGAR EL ARCHIVO ADMINISTRADORES"<< endl;
+        return false;
+    }
+
+    ifstream arch3 ("Clientes.txt");
+    string line3;
+
+    if(arch3.is_open()){
+        while(getline(arch3,line3)){
+            stringstream ss (line3);
+            string nombre;
+            getline(ss,nombre,';');
+            string apellido;
+            getline(ss,apellido,';');
+            string ciudad;
+            getline(ss,ciudad,';');
+            string idCliente;
+            getline(ss,idCliente,';');
+            string telefono;
+            getline(ss,telefono,';');
+            Cliente *nuevoCliente = new Cliente(nombre,apellido,ciudad,stoi(telefono),stoi(idCliente));
+            while(!ss.eof()){
+
+                string idEvento;
+                getline(ss,idEvento,';');
+                //TODO: FALTA AGREGAR EVENTOS A LA LISTA DE EVENTOS DEL ADMINISTRADOR
+
+            }
+
+            //TODO:SLicing
+            this->clientes->addPersona(*nuevoCliente);
+
+
+
+        }
+    }
+    else{
+        return false;
+    }
+
+
+
+
+    return true;
+
+
 }
